c7d4a053ce5636c2b4c18875f866c132c45d299a
Chris Nuernberger <chris@techascent.com>
2019-12-15 19:06:13 -0700

    Working through detection.

--- src/facial_rec/core.clj
+++ src/facial_rec/core.clj
@@ -1,6 +1,11 @@
-(ns facial-rec.core)
+(ns facial-rec.core
+  (:require [libpython-clj.require :refer [require-python]]
+            [libpython-clj.python :as py]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(require-python 'pyximport)
+(pyximport/install )
+(require-python 'cv2)
+(require-python '[rcnn.cython :as rc])
+(require-python '[rcnn.cython.bbox :as rc])
+(require-python '[retinaface :as rface :reload])
